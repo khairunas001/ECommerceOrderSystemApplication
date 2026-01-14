d7216ffb0dba7c3e999062a002bd62c0690e88d7
khairunas001 <khairunas2020@mail.ugm.ac.id>
2026-01-14 23:42:41 +0700

    first commit

---
+++ src/main/java/e_commerce_order_system/e_commerce_order_system/ECommerceOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package e_commerce_order_system.e_commerce_order_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceOrderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceOrderSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/e_commerce_order_system/e_commerce_order_system/ECommerceOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package e_commerce_order_system.e_commerce_order_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceOrderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
