b54cabf800da91a1921ff25618595b7b581130d4
khairunas001 <khairunas2020@mail.ugm.ac.id>
2026-01-18 12:36:14 +0700

    add test for product and transaction controllers v2.1

--- src/main/java/e_commerce_order_system/e_commerce_order_system/repository/ProductRepository.java
+++ src/main/java/e_commerce_order_system/e_commerce_order_system/repository/ProductRepository.java
@@ -1,4 +1,19 @@
 package e_commerce_order_system.e_commerce_order_system.repository;
 
-public class ProductRepository {
+import e_commerce_order_system.e_commerce_order_system.entity.Product;
+import jakarta.persistence.LockModeType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, String> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<Product> findById(String id);
+
 }
